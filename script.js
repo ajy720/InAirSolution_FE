e7c2d8b3a27ad5b73412c5f50878fb6583f95a43
AnHyeonSeok <ajy720@gmail.com>
2020-09-10 14:45:31 +0900

    air condition, weather icon

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 class Weather {
     main = null
+    id = null
     pm10 = null
     pm25 = null
     inAir = null
@@ -7,8 +8,44 @@ class Weather {
     temp_min = null
 
     setWeather() {
-        $("#weather").text(this.main)
+        this.setMain();
         $("#temperature").text(`${this.temp_max}º / ${this.temp_min}º`)
+    }
+
+    setMain() {
+        this.main = "맑음"
+        console.log(this.id)
+        switch (parseInt(this.id / 100)) {
+            case 2:
+            case 3:
+            case 5:
+                this.main = "비"
+                $("#weatherIcon").attr("src", "./img/rainny.png")
+                break
+
+            case 6:
+                this.main = "눈"
+                $("#weatherIcon").attr("src", "./img/snowy.png")
+                break
+
+            case 8:
+                if (this.id % 100 > 0) {
+                    this.main = "흐림"
+                    $("#weatherIcon").attr("src", "./img/cloud.png")
+                } else {
+                    this.main = "맑음"
+                    $("#weatherIcon").attr("src", "./img/sunny.png")
+                }
+                break
+
+            default:
+                this.main = "흐림"
+                $("#weatherIcon").attr("src", "./img/cloud.png")
+                break
+
+
+        }
+        $("#weather").text(this.main)
     }
 
     setAir() {
@@ -21,19 +58,19 @@ class Weather {
         switch (grade) {
             case 1:
                 el.text("좋음")
-                el.addClass("text-primary")
+                el.attr("class", "text-primary font-weight-bold")
                 break
             case 2:
                 el.text("보통")
-                el.addClass("text-info")
+                el.attr("class", "text-info font-weight-bold")
                 break
             case 3:
                 el.text("나쁨")
-                el.addClass("text-warning")
+                el.attr("class", "text-warning font-weight-bold")
                 break
             case 4:
                 el.text("매우 나쁨")
-                el.addClass("text-danger")
+                el.attr("class", "text-danger font-weight-bold")
                 break
         }
     }
@@ -42,11 +79,11 @@ class Weather {
 $(document).ready(function () {
     state = new Weather()
 
-    $("#open").click(()=>{
+    $("#open").click(() => {
         postCmd(true);
     })
 
-    $("#close").click(()=>{
+    $("#close").click(() => {
         postCmd(false);
     })
 
@@ -55,7 +92,7 @@ $(document).ready(function () {
     setInterval(() => {
         getWeather();
 
-    }, 5000)
+    }, 3000)
 })
 
 function getWeather(city = "종로구") {
@@ -72,14 +109,16 @@ function getWeather(city = "종로구") {
         method: "get",
         success: ({ weather, main }) => {
             // console.log(main)
-            state.main = weather[0].main
+            // state.main = weather[0].main    
+            state.id = weather[0].id
+            state.id = 701
             state.temp_max = parseInt(main.temp_max - 273)
             state.temp_min = parseInt(main.temp_min - 273)
 
             state.setWeather()
         }
     })
-    
+
     $.ajax({
         url: url2,
         method: "get",
@@ -88,7 +127,7 @@ function getWeather(city = "종로구") {
             state.pm10 = data.pm10
             state.pm25 = data.pm25
             state.inAir = data.quality
-            
+
             state.setAir()
         }
     })
@@ -96,15 +135,15 @@ function getWeather(city = "종로구") {
     console.log(state)
 }
 
-function postCmd(state){
+function postCmd(state) {
     url = `http://ec2-13-125-251-11.ap-northeast-2.compute.amazonaws.com:8000/`
-    
+
     $.ajax({
         url: url + ((state) ? "open" : "close"),
         method: "post",
 
     })
-    
+
     console.log(state)
-    
+
 }
